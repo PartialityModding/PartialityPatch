914c85766cefb73ce13fc43e06758aff3303952d
RiokuTheSlayer <RiokuTheSlayer@gmail.com>
2018-01-09 23:42:33 -0600

    First push
    Hooking through the static constructor of the Object class.

---
+++ PartialityPatch/Patches/Hooks/patch_Object.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Partiality;
+
+namespace UnityEngine.mm.Patches.Hooks {
+    [MonoMod.MonoModPatch("global:UnityEngine.Object")]
+    class patch_Object {
+
+        public extern void orig_ctor_Object();
+        [MonoMod.MonoModConstructor]
+        public void ctor_Object() {
+            orig_ctor_Object();
+
+            Debug.Log("Let's hook some code!");
+            PartialityManager.CreateInstance();
+        }
+    }
+}
